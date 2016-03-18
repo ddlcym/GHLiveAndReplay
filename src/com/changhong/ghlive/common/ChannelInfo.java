0a3119c8aae6829d40c72f76e8e766d28c22ada5
ddlcym <375843203@qq.com>
2016-03-18 17:13:28 +0800

    channel info

--- src/com/changhong/ghlive/common/ChannelInfo.java
+++ src/com/changhong/ghlive/common/ChannelInfo.java
@@ -1,5 +1,225 @@
 package com.changhong.ghlive.common;
 
+/*Channel Infomation*/
+/*Author: OscarChang*/
 public class ChannelInfo {
+
+	/* information about channels */
+	private String channelID;
+	private String channelName;
+	private String channelCode;
+	private String description;
+	private String videoType;
+	private String feeType;
+	private String resourceOrder;
+	private String resourceCode;
+	private String channelType;
+	private String cityCode;
+	private String gradeCode;
+	private String channelSpec;
+	private String networkId;
+	private String TSID;
+	private String serviceid;
+	private String assetID;
+	private String providerID;
+	private String posterInfo;
+	private String channelTypes;
+	private String channelNumber;
+	private String frequency;
+	private String symbolRate;
+	private String modulation;
+	private String platform;
+
+	public String getChannelID() {
+		return channelID;
+	}
+
+	public void setChannelID(String channelID) {
+		this.channelID = channelID;
+	}
+
+	public String getChannelName() {
+		return channelName;
+	}
+
+	public void setChannelName(String channelName) {
+		this.channelName = channelName;
+	}
+
+	public String getChannelCode() {
+		return channelCode;
+	}
+
+	public void setChannelCode(String channelCode) {
+		this.channelCode = channelCode;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getVideoType() {
+		return videoType;
+	}
+
+	public void setVideoType(String videoType) {
+		this.videoType = videoType;
+	}
+
+	public String getFeeType() {
+		return feeType;
+	}
+
+	public void setFeeType(String feeType) {
+		this.feeType = feeType;
+	}
+
+	public String getResourceOrder() {
+		return resourceOrder;
+	}
+
+	public void setResourceOrder(String resourceOrder) {
+		this.resourceOrder = resourceOrder;
+	}
+
+	public String getResourceCode() {
+		return resourceCode;
+	}
+
+	public void setResourceCode(String resourceCode) {
+		this.resourceCode = resourceCode;
+	}
+
+	public String getChannelType() {
+		return channelType;
+	}
+
+	public void setChannelType(String channelType) {
+		this.channelType = channelType;
+	}
+
+	public String getCityCode() {
+		return cityCode;
+	}
+
+	public void setCityCode(String cityCode) {
+		this.cityCode = cityCode;
+	}
+
+	public String getGradeCode() {
+		return gradeCode;
+	}
+
+	public void setGradeCode(String gradeCode) {
+		this.gradeCode = gradeCode;
+	}
+
+	public String getChannelSpec() {
+		return channelSpec;
+	}
+
+	public void setChannelSpec(String channelSpec) {
+		this.channelSpec = channelSpec;
+	}
+
+	public String getNetworkId() {
+		return networkId;
+	}
+
+	public void setNetworkId(String networkId) {
+		this.networkId = networkId;
+	}
+
+	public String getTSID() {
+		return TSID;
+	}
+
+	public void setTSID(String tSID) {
+		TSID = tSID;
+	}
+
+	public String getServiceid() {
+		return serviceid;
+	}
+
+	public void setServiceid(String serviceid) {
+		this.serviceid = serviceid;
+	}
+
+	public String getAssetID() {
+		return assetID;
+	}
+
+	public void setAssetID(String assetID) {
+		this.assetID = assetID;
+	}
+
+	public String getProviderID() {
+		return providerID;
+	}
+
+	public void setProviderID(String providerID) {
+		this.providerID = providerID;
+	}
+
+	public String getPosterInfo() {
+		return posterInfo;
+	}
+
+	public void setPosterInfo(String posterInfo) {
+		this.posterInfo = posterInfo;
+	}
+
+	public String getChannelTypes() {
+		return channelTypes;
+	}
+
+	public void setChannelTypes(String channelTypes) {
+		this.channelTypes = channelTypes;
+	}
+
+	public String getChannelNumber() {
+		return channelNumber;
+	}
+
+	public void setChannelNumber(String channelNumber) {
+		this.channelNumber = channelNumber;
+	}
+
+	public String getFrequency() {
+		return frequency;
+	}
+
+	public void setFrequency(String frequency) {
+		this.frequency = frequency;
+	}
+
+	public String getSymbolRate() {
+		return symbolRate;
+	}
+
+	public void setSymbolRate(String symbolRate) {
+		this.symbolRate = symbolRate;
+	}
+
+	public String getModulation() {
+		return modulation;
+	}
+
+	public void setModulation(String modulation) {
+		this.modulation = modulation;
+	}
+
+	public String getPlatform() {
+		return platform;
+	}
+
+	public void setPlatform(String platform) {
+		this.platform = platform;
+	}
 
 }
